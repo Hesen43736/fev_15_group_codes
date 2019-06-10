1f7afbe51cfff48ac3582aec6fe7f0b9db035f20
Yusif Alizada <yusifedu@gmail.com>
2019-06-10 18:05:46 +0400

    create table

---
+++ ders13/js/app.js
@@ -0,0 +1,27 @@
+function saveTask(){
+ var inputElement=document.getElementById("task");
+ var text=inputElement.value;
+ var text1=document.createTextNode(text);
+ var p1=document.createElement("li");
+ p1.appendChild(text1);
+ var parent=document.getElementById("tasks");
+ parent.appendChild(p1);
+ }
+ 
+ function saveHuman(){
+ var nameElement=document.getElementById("name");
+ var name=nameElement.value;
+ 
+ var salaryElement=document.getElementById("salary");
+ var salary=salaryElement.value;
+ var tr=document.createElement("tr");
+ var td1=document.createElement("td");
+ td1.innerHTML=name;
+ 
+ var td2=document.createElement("td");
+ td2.innerHTML=salary;
+ tr.appendChild(td1);
+ tr.appendChild(td2);
+ var parent=document.getElementById("humans");
+ parent.appendChild(tr);
+ }
