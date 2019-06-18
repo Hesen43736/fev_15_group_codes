bb72fec88251ddce823e37c6662e9f7214cf0686
Yusif Alizada <yusifedu@gmail.com>
2019-06-18 09:07:39 +0400

    ders 15 calc step 1

---
+++ ders15/app.js
@@ -0,0 +1,39 @@
+/*
+function myFunction(){
+	
+var car={color:"red",model:"H1"};
+	
+	var person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+	
+	var x=typeof person.age;
+	var p1=document.getElementById('p1');
+	p1.innerHTML=x;
+	
+	var person = {
+  firstName: "John",
+  lastName : "Doe",
+  id       : 5566,
+  fullName : function() {
+    return this.firstName + " " + this.lastName;
+  }
+};*/
+
+
+function numberKey(elem){
+	
+var result=elem.innerHTML;
+var display=document.getElementById('result');
+display.innerHTML=result;
+	  
+}
+
+
+
+
+
+
+
+
+
+
+	 
