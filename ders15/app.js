0435e2a62fd1fa5550e9a958059f509a98846f60
Yusif Alizada <yusifedu@gmail.com>
2019-06-21 18:31:44 +0400

    ders 16

--- ders15/app.js
+++ ders15/app.js
@@ -22,15 +22,53 @@ var car={color:"red",model:"H1"};
 function numberKey(elem){
 	
 var result=elem.innerHTML;
+result=result.trim();
 var display=document.getElementById('result');
-display.innerHTML=result;
+display.innerHTML+=result;
 	  
 }
 
-
-
-
-
+function clearScreen(){
+	
+	document.getElementById('result').innerHTML="";
+	
+}
+function deleteKey(){
+	
+	var result=document.getElementById('result').innerHTML;
+	var lengthOfResult=result.length;
+	if(lengthOfResult==0){}else{// 1234
+		result=result.substr(0,lengthOfResult-1);
+		document.getElementById('result').innerHTML=result;
+	}
+	
+}
+var endResultGlobal;
+function calculate(){
+	
+	var result=document.getElementById('result');
+	var resultText=result.innerHTML;
+	// evaulation function eval ()
+    var endResult=eval(resultText);
+    result.innerHTML =resultText+' = '+ endResult;	
+	 
+	
+}
+function memory(){
+	var result=document.getElementById("result");
+	var resultText=result.innerHTML;
+	var array=resultText.split("=");
+	var endResult=array[1];
+	endResultGlobal=endResult;
+	
+}
+function memoryReturn(){
+		var result=document.getElementById("result");
+	var resultText=result.innerHTML;
+	result.innerHTML=resultText+endResultGlobal;
+	
+	
+}
 
 
 
