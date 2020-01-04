4be52637f79ba7da4df2d8725fcb0f2764853b1e
qrup1 <yusifedu@gmail.com>
2020-01-03 20:33:36 -0800

    routing used

--- projects/frontend/todolistfront/src/app/app-routing.module.ts
+++ projects/frontend/todolistfront/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './component/login/login.component';
+import { TodoListComponent } from './component/todo-list/todo-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:LoginComponent},
+  {path:'login',component:LoginComponent},
+  {path:'todos',component:TodoListComponent},
+  {path:'**',component:LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
