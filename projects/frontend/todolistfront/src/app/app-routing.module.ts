ddc3a29559043fb158faafdace20c04cf46aa070
yalizada <37800222+yalizada@users.noreply.github.com>
2020-01-10 08:17:26 -0800

    route guard used

--- projects/frontend/todolistfront/src/app/app-routing.module.ts
+++ projects/frontend/todolistfront/src/app/app-routing.module.ts
@@ -3,13 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './component/login/login.component';
 import { TodoListComponent } from './component/todo-list/todo-list.component';
 import { LogoutComponent } from './component/logout/logout.component';
+import { RouteGuardService } from './service/route-guard.service';
 
 
 const routes: Routes = [
-  {path:'',component:LoginComponent,canActivate:[]},
+  {path:'',component:LoginComponent },
   {path:'login',component:LoginComponent},
-  {path:'logout',component:LogoutComponent},
-  {path:'todos',component:TodoListComponent,canActivate:[]},
+  {path:'logout',component:LogoutComponent,canActivate:[RouteGuardService]},
+  {path:'todos',component:TodoListComponent,canActivate:[RouteGuardService]},
   {path:'**',component:LoginComponent}
 ];
 
--- projects/frontend/todolistfront/src/app/service/route-guard.service.ts
+++ projects/frontend/todolistfront/src/app/service/route-guard.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { UserService } from './user.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RouteGuardService {
+export class RouteGuardService implements CanActivate {
 
-  constructor() { }
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+
+    let result: boolean = false;
+    
+if(sessionStorage.getItem('authorization')){
+  result=true;
+}else{
+  this.router.navigate(['login']);
+}
+
+    return result;
+
+  }
+  constructor(private router: Router ) { }
+  
 }
