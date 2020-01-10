98917cf8097a8d66482f8f6aca8ce0d5baee7d7d
yalizada <37800222+yalizada@users.noreply.github.com>
2020-01-10 08:30:55 -0800

    logout

--- projects/frontend/todolistfront/src/app/component/logout/logout.component.ts
+++ projects/frontend/todolistfront/src/app/component/logout/logout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-logout',
@@ -7,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
     sessionStorage.clear();
+
+
+    setTimeout(() => {
+      this.router.navigate(['login']);
+    }, 1000);
   }
 
 }
