66487f970477f3f1441a106ab660d908607bf60d
yalizada <37800222+yalizada@users.noreply.github.com>
2020-01-13 07:59:12 -0800

    Update BookRestController.java
    Hibernate : find all and find by id

--- bookingapp/back/booking/src/main/java/az/developia/booking/controller/BookRestController.java
+++ bookingapp/back/booking/src/main/java/az/developia/booking/controller/BookRestController.java
@@ -1,7 +1,10 @@
 package az.developia.booking.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,4 +33,17 @@ public class BookRestController {
 		bookDAO.save(book);
 		
 	}
+	
+	
+	@GetMapping(path="/find-all")
+	public List<Book> findAll(){ 
+	return	bookDAO.findAll();
+		
+	}
+	@GetMapping(path="/find-by-id/{id}")
+	public Book  findById(@PathVariable(name="id") Integer id){ 
+	return	bookDAO.findById(id).get();
+		
+	}
+	
 }
