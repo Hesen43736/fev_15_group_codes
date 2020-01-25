da45f4913b51b63731cf427597e755d83f369e7c
Yusif Alizada <yusifedu@gmail.com>
2020-01-24 19:57:11 -0800

    front project reset

--- bookingapp/front/bookingfront/src/app/app.component.ts
+++ bookingapp/front/bookingfront/src/app/app.component.ts
@@ -7,31 +7,6 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-   
-constructor(private http:HttpClient){
-
+    
 }
-  
-imageSelected(event){
-console.log(event);
-  let myImage= event.target.files[0] ;
-  
-  let  fd:FormData=new FormData();
-  fd.append('file',myImage);
-  
-  this.http.post<ImageBean>('http://localhost:8067/fileupload',fd).subscribe(
-  resp=>{
-  console.log(resp.image);
-  this.yuklenen=resp.image;
-  }
-  );
-  }
-
-  yuklenen:string;
-
-}
-
-export class ImageBean{
-  public id:number;
-  public image:string;
-}
+ 
