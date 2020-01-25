40443617a4014a7cdd3433e6e43ee9015f5809c6
Yusif Alizada <yusifedu@gmail.com>
2020-01-24 20:58:18 -0800

    Update BookRestController.java

--- bookingapp/back/booking/src/main/java/az/developia/booking/controller/BookRestController.java
+++ bookingapp/back/booking/src/main/java/az/developia/booking/controller/BookRestController.java
@@ -3,6 +3,7 @@ package az.developia.booking.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +17,7 @@ import az.developia.booking.model.Book;
 
 @RestController
 @RequestMapping(path = "/books")
-
+@CrossOrigin(origins="*")
 public class BookRestController {
 
 	@Autowired
