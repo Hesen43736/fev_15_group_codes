c4412409ae74b26bf9443d57646c37b878cb4a70
Yusif Alizada <yusifedu@gmail.com>
2020-01-24 20:26:54 -0800

    routing step 1

--- bookingapp/front/bookingfront/src/app/app-routing.module.ts
+++ bookingapp/front/bookingfront/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './component/login/login.component';
+import { LogoutComponent } from './component/logout/logout.component';
+import { BookListComponent } from './component/book-list/book-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login',component:LoginComponent},
+  {path:'logout',component:LogoutComponent},
+  {path:'books',component:BookListComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
