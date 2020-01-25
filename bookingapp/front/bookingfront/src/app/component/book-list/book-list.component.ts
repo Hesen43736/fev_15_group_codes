28e8bafd61d3f8165ff07189ee74d6952c9a25b8
Yusif Alizada <yusifedu@gmail.com>
2020-01-24 21:04:56 -0800

    image show

--- bookingapp/front/bookingfront/src/app/component/book-list/book-list.component.ts
+++ bookingapp/front/bookingfront/src/app/component/book-list/book-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { BookService } from 'src/app/service/book.service';
 import { Book } from 'src/app/model/book';
+import { API_URL } from 'src/app/constants';
 
 @Component({
   selector: 'app-book-list',
@@ -11,8 +12,9 @@ import { Book } from 'src/app/model/book';
 export class BookListComponent implements OnInit {
 books:Book[]=[];
   constructor(private router:Router,private bookService:BookService) { }
-
+download:string='';
   ngOnInit() {
+    this.download=API_URL+"/filedownload/files/";
     this.bookService.findAll().subscribe(
       resp=>{
         this.books=resp;
