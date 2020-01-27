b8f48f0db76ce9ad19a9e9b159fecc5454d3dfea
Yusif Alizada <yusifedu@gmail.com>
2020-01-27 20:45:59 +0400

    Update question.service.ts

--- questionsApp/src/app/service/question.service.ts
+++ questionsApp/src/app/service/question.service.ts
@@ -5,27 +5,40 @@ import { Injectable } from '@angular/core';
 })
 export class QuestionService {
 
-  questions:string[]=[];
-  students:string[]=[];
+  questions: string[] = [];
+  students: string[] = [];
 
-  constructor() { 
+  constructor() {
 
-    this.students.push('Hüseyn Hüseynli','Hüseyn Nemətli',
-    'Hüseyn Tahirzadə','Əli Məmmədzadə','Fərid Salmanov',
-    'Ünsal Qasımlı','Məmmədcan Caniyev','Mahmud Əliyev');
+    this.students.push('Hüseyn Hüseynli', 'Hüseyn Nemətli',
+      'Hüseyn Tahirzadə', 'Əli Məmmədzadə', 'Fərid Salmanov',
+      'Ünsal Qasımlı', 'Məmmədcan Caniyev', 'Mahmud Əliyev');
 
 
     this.questions.push(
-    'Angular nədir?',
-    'Angular-ı necə quraq?',
-    'NPM nədir?',
-    'CLI nədir?',
-    'Angular-da yeni proyekt necə yaradılır?',
-    'Angular-da proyekt necə run edilir?',
-    'Angular-da proyekti fərqli port adresində necə run edirik?',
-    'String İnterpolation nədir?',
-    'main.ts faylı haqqında danış',
-    'Bootstrap-ı angulara necə qururlar?' );
+      'Angular nədir?',
+      'Angular-ı necə quraq?',
+      'NPM nədir?',
+      'CLI nədir?',
+      'Angular-da yeni proyekt necə yaradılır?',
+      'Angular-da proyekt necə run edilir?',
+      'Angular-da proyekti fərqli port adresində necə run edirik?',
+      'String İnterpolation nədir?',
+      'main.ts faylı haqqında danış',
+      'Bootstrap-ı angulara necə qururlar?', 
+      'Angular-ın hansı versiyaları var?',
+       'Angular üçün hansı proqramları qurmaq lazımdır?', 
+       'Typescript nədir?', 
+       'Typescript ilə javascript arasında nə fərq var?', 
+       'Typescript-də massivlər necə yaradılır?', 
+       'Typescript-də let və const nə üçündür?', 
+       'Typescript-də sinif necə yaradılır?',
+       'node_modules qovluğu nə üçündür?',
+       'angular.json faylı haqqında danış',
+       'package.json faylı haqqında danış',
+       'src qovluğu nə üçündür?',
+       'assets qovluğu nə üçündür?',
+       'app.modules.ts faylı haqqında danış');
 
   }
 }
